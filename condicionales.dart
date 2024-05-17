4a2542b32b3feb86b5006ab72cc9d4974d6294da
felipe alvarez <fe.alvarezdiaz@gmail.com>
2024-05-17 13:51:35 -0400

    finish conditions and add field loops

--- condicionales.dart
+++ condicionales.dart
@@ -3,10 +3,15 @@ void main() {
   int age = 121;
   bool vote = votefunction(age);
   String vote2 = votepalabra(age);
+  String vote3 = votefunctionresume(age);
   print(vote);
   print(vote2);
+  print(vote3);
 }
 
+/// estas son funciones que retornan un valor booleano y un string
+/// la funcion votefunction recibe un parametro de tipo int y retorna un valor booleano
+/// y dentro de estta funcion usamos los condicionales if-else
 bool votefunction(int age) {
   if (age >= 18 && age <= 120) {
     return true;
@@ -23,4 +28,16 @@ String votepalabra(int name) {
   } else {
     return "no puede votar";
   }
+}
+
+/// esta funcion es un poco mas resumida que la anterior y usa ? y : para simplificar el codigo
+String votefunctionresume(int age) {
+  String result = age >= 18 && age <= 100
+      ? "puede votar"
+      : age > 100
+          ? "estas seguro que estas vivo ?"
+          : age < -1
+              ? "no has nacido"
+              : "no puede votar";
+  return result;
 }
