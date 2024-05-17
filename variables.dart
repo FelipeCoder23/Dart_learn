b612c881ccd324b7a489b892363d5230b15296cc
felipe alvarez <fe.alvarezdiaz@gmail.com>
2024-05-17 01:24:25 -0400

    update file variables with new features

--- variables.dart
+++ variables.dart
@@ -5,7 +5,7 @@
 /// List<String> es una lista de strings
 /// Map<String, dynamic> es un mapa de datos donde la clave es un string y el valor puede ser de cualquier tipo de dato
 
-///first vara
+
 
 void main() {
   String name = "Akshit";
@@ -18,6 +18,30 @@ void main() {
     "male": male,
     "fruits": fruits
   };
+  /// a las listas con el . se le pueden realizar operaciones
+  /// las operaciones mas communes son add, remove, clear
 
+  fruits.add("banana");
+  /// la diferencia entre add y addAll es que add solo agrega un elemento a la lista y addAll agrega una lista completa
+  /// tbn se pueden ordenar las listas con sort por ejemplo fruits.sort();
+  fruits.sort();
+
+  /// otra funcion que se puede realizar es contains que verifica si un elemento esta en la lista
+  bool valor = fruits.contains("banana");
+  /// el where que se utiliza para filtrar los elementos de la lista
+  /// como ejemplo se puede filtrar los elementos que contienen la palabra mango y se almacena en la variable result
+  var result = fruits.where((element) => element.contains("mango"));
+  /// tbn se le puede aplica al final  .TOlist() para convertir el resultado en una lista
+  /// por ej fruits.where((element) => element.contains("mango")).toList();
+
+  /// con || y && se pueden realizar operaciones logicas el ej anterior quedaria asi
+  /// fruits.where((element) => element.contains("mango") && element = "apple").toList();
+  print(result);
+  print(fruits);
   print(myData);
+  print(myData["name"]);
+  /// se puede acceder a los elementos de un mapa con la clave
+
+  /// esto es para acceder a las claves de un mapa
+  print(myData.keys);
 }
